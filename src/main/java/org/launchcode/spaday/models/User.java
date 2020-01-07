5922e31c45c67fa1a632c1d37d83937653ffd858
Chris Carriere <chris.carriere323@gmail.com>
2020-01-06 19:24:20 -0600

    completed bonus, added password validation

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -2,6 +2,7 @@ package org.launchcode.spaday.models;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class User {
@@ -13,9 +14,19 @@ public class User {
     @Email
     private String email;
 
-    @NotBlank()
+    @NotBlank
     @Size(min = 6, message = "Password must be at least 6 characters!")
     private String password;
+
+    @NotNull(message = "Passwords do not match!")
+    private String verifyPassword;
+
+
+    private void checkPassword(){
+        if(password != null && verifyPassword != null && !verifyPassword.equals(password)){
+            verifyPassword = null;
+        }
+    }
 
     public User() {
 
@@ -50,5 +61,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
